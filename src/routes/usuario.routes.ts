f8001208ae5e0b2cf834b9a5ebcd77a5f6581781
Gustavo Farias <gustavocel82@gmail.com>
2022-02-11 11:03:56 -0300

    dependabot

--- src/routes/usuario.routes.ts
+++ src/routes/usuario.routes.ts
@@ -4,7 +4,7 @@ import verifyJwt from "../middlewares/verifyJwt.middlewares";
 
 const router = express.Router();
 
-router.route("/").get(verifyJwt, controller.getAll).post(controller.post);
+router.route("/").get(controller.getAll).post(controller.post);
 router.route("/:id").get(controller.getById);
 
 export default router;
